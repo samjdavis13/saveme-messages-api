6e33edf22eabb0f9a9b718faddec1b833c96a00f
Sam Davis <sam@sjd.co>
2016-04-24 22:46:38 +1000

    basic bootstrapping of web app

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 var appFolder = 'public/'
 
 gulp.task('compileSass', function() {
     // Compile Sass here
-    gulp.src(appFolder + 'scss/**/*.scss')
-        .pipe(sass())
-        .pipe(rename('style.css'))
-        .pipe(gulp.dest(appFolder + 'css'));
+    return gulp.src(appFolder + 'scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename('style.css'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(appFolder + 'css'));
 });
 
 gulp.task('default', function() {
