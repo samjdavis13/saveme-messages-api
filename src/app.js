90096bb1daa406d11999087b605ab1b7b90b3ca5
Sam Davis <sam@sjd.co>
2016-04-25 19:11:26 +1000

    add basic auth

--- src/app.js
+++ src/app.js
@@ -1,15 +1,42 @@
 'use strict';
 
+var config = require('./config.js');
+
+var basicAuth = require('basic-auth');
 var express = require('express');
 var parser = require('body-parser');
 var router = require('./api');
 
-
 require('./database');
-
 require('./seed.js');
 
 var app = express();
+
+// Authenticator
+app.use(function(req, res, next) {
+
+    // Allow GET requests on the /api/messages route
+    if (req.url === '/api/messages' && req.method.toUpperCase() === 'GET') {
+        return next();
+    };
+
+    function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.sendStatus(401);
+  };
+
+    var user = basicAuth(req);
+
+    if (!user || !user.name || !user.pass) {
+        return unauthorized(res);
+    };
+
+    if (user.name === config.user.name && user.pass === config.user.pass) {
+        return next();
+    } else {
+        return unauthorized(res);
+    };
+});
 
 app.use(parser.json());
 
@@ -17,6 +44,6 @@ app.use('/', express.static('public'));
 
 app.use('/api', router);
 
-app.listen(3000, function() {
-    console.log("The app is listening on port 3000");
+app.listen(config.port, function() {
+    console.log("The app is listening on port " + config.port);
 });
---
+++ src/config.example.js
@@ -0,0 +1,12 @@
+/**
+* @config Application configuration
+*/
+
+// Port the app wil run on
+module.exports.port = 3000;
+
+// Credentials used for basic http auth
+module.exports.user = {
+    name: "test",
+    pass: "test"
+}
